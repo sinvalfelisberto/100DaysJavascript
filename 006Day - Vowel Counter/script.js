abf8df4f514d429318b50a958aae2dd51dc273b4
sinvalfelisberto <sinvalfelisberto@gmail.com>
2023-02-10 06:27:01 -0300

    Aprendi como passar params em um listener

--- 006Day - Vowel Counter/script.js
+++ 006Day - Vowel Counter/script.js
@@ -1,19 +1,24 @@
  const result = document.querySelector('.result')
  const btn = document.querySelector('.btn')
  const word = document.querySelector('.input-text')
- 
 
+function mouseEmCima(testo) {
+    alert(testo)
+ }
 
- btn.addEventListener('click', countVowel)
-
- function countVowel() {
-    let vowelCount = 0
+ btn.addEventListener('click', (event) => {
     let wordValue = word.value.toLowerCase()
+    let retorno = countVowel(wordValue)
+    result.innerHTML = retorno
+ })
+
+ function countVowel(wordValue) {
+    let vowelCount = 0
     for (let i = 0; i < wordValue.length; i++){
         let letter = wordValue[i]
         if (letter.match(/([a,e,i,o,u])/)) {
             vowelCount++
         }
     }
-    result.innerHTML = `${wordValue.toUpperCase()} contains ${vowelCount} vowel(s)`
+    return `${wordValue.toUpperCase()} contains ${vowelCount} vowel(s)`
  }
