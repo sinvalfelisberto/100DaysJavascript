a67adb6c5dfbfcd82b87b07ee951c7ea03428e44
sinvalfelisberto <sinvalfelisberto@gmail.com>
2023-02-25 06:03:33 -0300

    atualizando o projeto

--- 007 - Copy Paste/script.js
+++ 007 - Copy Paste/script.js
@@ -1,13 +1,21 @@
- const btn = document.querySelector('.btn')
- const coupon = document.querySelector('.coupon')
- 
- const copyText = (e) => {
-    e.preventDefault()
-    let couponLen = coupon.value.length
-    coupon.select()
-    coupon.setSelectionRange(0, couponLen)
-    document.execCommand('copy')
+const btn = document.querySelector('.btn')
+const coupon = document.querySelector('.coupon')
 
- }
-
- btn.addEventListener('click', copyText)
+const copyText = (e) => {
+   e.preventDefault()
+   let couponLen = coupon.value.length
+  //  coupon.select()
+  //  coupon.setSelectionRange(0, couponLen)
+   
+   navigator.clipboard.writeText(coupon.value)
+     .then(() => {
+        btn.textContent = "Copied!!!"
+        setTimeout(() => {
+           btn.textContent = "Copy"
+        }, 1500)
+     })
+   
+   //document.execCommand('copy')
+   
+}
+btn.addEventListener('click', copyText)
